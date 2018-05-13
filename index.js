9ffa5342882552b32a8656bc87d29e1f45e0a1d6
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-13 05:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function updateObjectWithKeyAndValue(obj,key,value){
 function updateObjectWithKeyAndValue(obj,key,value){
 	return Object.assign({}, obj, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+	return obj[key]=value;
+}
